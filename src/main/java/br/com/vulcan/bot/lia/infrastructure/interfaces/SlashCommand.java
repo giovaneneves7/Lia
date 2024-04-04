933c14614ce89b9f6b883ea5da2b9d7c81e7103e
giovaneneves7 <autornekoyasha@gmail.com>
2024-04-04 14:37:02 -0300

    feat: Cria diretório 'interfaces' no pacote de infraestrutura

---
+++ src/main/java/br/com/vulcan/bot/lia/infrastructure/interfaces/SlashCommand.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.vulcan.bot.lia.infrastructure.interfaces;
+
+/**
+ *
+ * @author Giovane Neves
+ * @since V1.0.3
+ */
+public interface SlashCommand {
+    
+}
